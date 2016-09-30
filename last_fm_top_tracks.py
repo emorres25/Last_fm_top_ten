23e0733a1ae1335c29fce8ff6a96d91780055ddf
Shaurya <Shaurya@Shauryas-MacBook-Pro.local>
2016-10-01 01:46:17 +0530

    Updated to new Last.fm

--- last_fm_top_tracks.py
+++ last_fm_top_tracks.py
@@ -1,37 +1,13 @@
-import urllib2
-from bs4 import BeautifulSoup
-import os
+import requests
+import json
 
-stdurl = "https://www.last.fm/music/"
-band = raw_input("Enter the name of the band: ").split(' ')
+last_fm_api_key  = 'fa15d1b3850fd471297957941d4839f4'
 
-furl = stdurl
-for part in range(0,len(band)):
-	if(part==len(band)-1):
-		furl = furl + band[part]
-	else:
-		furl = furl + band[part] + '+'
-
-print furl + "\n"
-
-res = urllib2.urlopen(furl)
-
-page = res.read()
-
-splitted_page = page.split("<thead class=\"sr-only\">", 1)
-splitted_page = splitted_page[1].split("</thead>", 1)
-splitted_page = splitted_page[1].split("</tbody>", 1)
-
-soup = BeautifulSoup(splitted_page[0], 'html5lib')
-#print soup.prettify()
-os.system('cls')
-print """ -------------------------------------------------------------------------------
-			THE TOP TRACKS OF THE BAND ARE:
- -------------------------------------------------------------------------------
-		  """	
-i=1
-for parts in soup.find_all('a', class_="link-block-target"):
-	this = parts.get('title')
-	print str(i) + '. ' + str(this)
-	i+=1
-	
+artist = raw_input()
+callback_url = 'http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=' + artist +'&api_key='+last_fm_api_key+'&limit=10&format=json'
+i=0
+r = requests.get(callback_url)
+data = json.loads(r.text)
+#print data
+for i in range(0,10):
+    print data['toptracks']['track'][i]['name']
